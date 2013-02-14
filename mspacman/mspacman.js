9853f063ebd3fb0361c5fa2d1251ecabd7c2dffd
amende <andrew.mendelsohn@tufts.edu>
2013-02-14 11:28:20 -0500

    Put ghost/mspac in the google O's

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -7,7 +7,8 @@ function start_game() {
 		pac = new Image();
 		pac.src = 'pacman10-hp-sprite.png';
 		ctx.drawImage(pac, 320, 0, 465, 137, 0, 0, 465, 137);
-		
+		ctx.drawImage(pac, 80, 0, 20, 20, 148, 60, 20, 20);
+		ctx.drawImage(pac, 100, 140, 20, 20, 212, 60, 20, 20);
 	}
 	else {
 		alert('Sorry, canvas is not supported on your browser!');
