1b54cd6920001cb96b6085ba048fe5e8a6ab98d1
amende <andrew.mendelsohn@tufts.edu>
2013-02-15 16:59:23 -0500

    Frogger logs and cars now draw with loop and array

--- frogger/game.js
+++ frogger/game.js
@@ -14,13 +14,21 @@ function draw() {
 		ctx.fillRect(0, 0, 399, 283); //water
 		ctx.fillStyle = "#000000";
 		ctx.fillRect(0, 283, 399, 282); //road
+		
 		ctx.drawImage(img, 0, 0, 399, 100, 0, 0, 399, 100); //title+green
 		ctx.drawImage(img, 0, 119, 399, 34, 0, 280, 399, 34); //roadside-mid
 		ctx.drawImage(img, 0, 119, 399, 34, 0, 480, 399, 34); //roadside-bot
 		ctx.drawImage(img, 45, 367, 25, 25, 180, 485, 25, 25); //frog-start
-		ctx.drawImage(img, 7, 197, 120, 25, 200, 200, 120, 25); //log
-		ctx.drawImage(img, 10, 267, 28, 20, 100, 400, 28, 20); //car1
-		ctx.drawImage(img, 105, 302, 45, 18, 180, 440, 45, 18); //car2
+
+		for (i=0; i<vehicles.length; i++) { //draw cars
+		c = vehicles[i];
+		ctx.drawImage(img, c.sx, c.sy, c.w, c.h, c.x, c.y, c.w, c.h);
+		}
+		for (i=0; i<logs.length; i++) { //draw logs
+		l = logs[i];
+		ctx.drawImage(img, l.sx, l.sy, l.w, l.h, l.x, l.y, l.w, l.h);
+		}
+		
 		draw_text();
 	}
 	else {
@@ -37,19 +45,21 @@ function init_game_vals() {
 	game_over = false;
 	level = 1;
 	time = 100;
-	vehicles = [{"x":100, "y":400, "spd":10},
-							{"x":180, "y":400, "spd":10}];
-	logs = [{"x":200, "y":200, "spd":10}];
+	vehicles = [{"sx":10, "sy":267, "w":28, "h":20, "x":100, "y":400, "spd":10},
+							{"sx":105, "sy":302, "w":45, "h":18, "x":180, "y":440, "spd":10}];
+	logs = [{"sx":7, "sy":197, "w":120, "h":25, "x":200, "y":200, "spd":10}];
 }
 
 function draw_text() {
-	ctx.font="14px Georgia";
+	ctx.font="14px Helvetica";
 	ctx.fillStyle="rgb(0,255,0)";
-	ctx.fillText("Score: "+score+ "       Highscore: "+high_score,10,555);
+	ctx.fillText("Score: "+score,10,555);
+	ctx.fillText("Highscore: "+high_score,100,555);
+
 	for (i=0; i<lives; i++) {
 		ctx.drawImage(img, 13, 334, 17, 23, 10+i*22, 515, 17, 23);
 	}
-	ctx.font="20px Georgia";
+	ctx.font="20px Helvetica";
 	ctx.fillStyle="rgb(0,255,0)";
 	ctx.fillText("Level "+level, 90, 535);
 }//13 334 17 23
