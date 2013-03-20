be38eda107ea0d79f57b3ddef9a6b0cf02cf0e52
amende <andrew.mendelsohn@tufts.edu>
2013-03-19 23:23:22 -0400

    Frogger: cars animated, arrow input moves frog

--- frogger/game.js
+++ frogger/game.js
@@ -6,10 +6,33 @@ function start_game() {
 		ctx = canvas.getContext('2d');
 		init_game_vals();
 		draw_all();
+		start_controls();
+		setInterval(game_loop, 12);
 	}
 	else {
 		alert('Sorry, canvas is not supported on your browser!');
 	}
+}
+
+function start_controls() {
+	document.addEventListener("keydown", move_frog);
+}
+
+function move_frog (input) {
+	input = input.keyCode;
+	if (input == 37 && frog.x > 20)
+		frog.x -= 28;
+	else if (input == 38 && frog.y > 20)
+		frog.y -= 33.5;
+	else if (input == 39 && frog.x < 380)
+		frog.x += 28;
+	else if (input == 40 && frog.y < 485)
+		frog.y += 33.5;
+}
+
+function game_loop() {
+	update_game_vals();
+	draw_all();
 }
 
 function draw_all() {
@@ -28,8 +51,6 @@ function draw_all() {
 }
 
 function draw_movables() {
-	ctx.drawImage(img, 45, 367, 25, 25, frog.x, frog.y, 25, 25); //frog
-
 	for (i=0; i<vehicles.length; i++) { //draw cars
 		c = vehicles[i];
 		ctx.drawImage(img, c.sx, c.sy, c.w, c.h, c.x, c.y, c.w, c.h);
@@ -38,7 +59,8 @@ function draw_movables() {
 		l = logs[i];
 		ctx.drawImage(img, l.sx, l.sy, l.w, l.h, l.x, l.y, l.w, l.h);
 	}
-	
+
+	ctx.drawImage(img, 45, 367, 25, 25, frog.x, frog.y, 25, 25); //frog
 	draw_footer();	
 }
 
@@ -49,10 +71,48 @@ function init_game_vals() {
 	game_over = false;
 	level = 1;
 	time = 100;
-	frog = {"x":180, "y":485};
-	vehicles = [{"sx":10, "sy":267, "w":28, "h":20, "x":100, "y":400, "spd":10},
-							{"sx":105, "sy":302, "w":45, "h":18, "x":180, "y":440, "spd":10}];
-	logs = [{"sx":7, "sy":197, "w":120, "h":25, "x":200, "y":200, "spd":10}];
+	frog = {"x":185, "y":485};
+	vehicles = [
+			//yellow racer
+				{"sx":80, "sy":264, "w":24, "h":27, "x":80,  "y":452, "spd":-.6},
+				{"sx":80, "sy":264, "w":24, "h":27, "x":200, "y":452, "spd":-.6},
+				{"sx":80, "sy":264, "w":24, "h":27, "x":320, "y":452, "spd":-.6},
+
+			//bulldozer
+				{"sx":10, "sy":300, "w":27, "h":24, "x":100, "y":418, "spd":.8},
+				{"sx":10, "sy":300, "w":27, "h":24, "x":220, "y":418, "spd":.8},
+				{"sx":10, "sy":300, "w":27, "h":24, "x":340, "y":418, "spd":.8},
+
+			//pink
+				{"sx":10, "sy":267, "w":28, "h":20, "x":20,  "y":384, "spd":-1},
+				{"sx":10, "sy":267, "w":28, "h":20, "x":140, "y":384, "spd":-1},
+				{"sx":10, "sy":267, "w":28, "h":20, "x":260, "y":384, "spd":-1},
+
+			//white racer
+				{"sx":46, "sy":264, "w":27, "h":25, "x":60,  "y":350, "spd":.8},
+				{"sx":46, "sy":264, "w":27, "h":25, "x":180, "y":350, "spd":.8},
+				{"sx":46, "sy":264, "w":27, "h":25, "x":300, "y":350, "spd":.8},
+
+
+			//]truck
+				{"sx":105, "sy":302, "w":45, "h":18, "x":0,   "y":316, "spd":-.8},
+				{"sx":105, "sy":302, "w":45, "h":18, "x":150, "y":316, "spd":-.8}
+				]
+	logs = [{"sx":7, "sy":197, "w":120, "h":25, "x":200, "y":200, "spd":-1}];
+}
+
+function update_game_vals() {
+	for (var i = 0; i < vehicles.length; i++) {
+		vehicles[i].x = (vehicles[i].x + vehicles[i].spd) % 400;
+		if (vehicles[i].x <= 0 - vehicles[i].w)
+			vehicles[i].x = 400;
+	};
+
+	for (var i = 0; i < logs.length; i++) {
+		logs[i].x = (logs[i].x + logs[i].spd) % 400;
+		if (logs[i].x <= 0 - logs[i].w)
+			logs[i].x = 400;
+	};
 }
 
 function draw_footer() {
